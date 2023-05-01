981346415b9b27c48bbc7c450a0b042c6e18faa8
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-01 14:02:07 +0200

    Modify controller and service - controller now returns DTO with only necessary data

--- src/main/java/dev/xalpol12/wheretoeatbackend/service/PlacesService.java
+++ src/main/java/dev/xalpol12/wheretoeatbackend/service/PlacesService.java
@@ -5,37 +5,50 @@ import com.google.maps.ImageResult;
 import com.google.maps.PlacesApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.*;
+import dev.xalpol12.wheretoeatbackend.service.dto.PhotoResponseDTO;
 import dev.xalpol12.wheretoeatbackend.service.dto.PlaceRequestDTO;
 import dev.xalpol12.wheretoeatbackend.service.dto.PhotoRequestDTO;
+import dev.xalpol12.wheretoeatbackend.service.dto.PlaceResponseDTO;
+import dev.xalpol12.wheretoeatbackend.service.mapper.PhotoMapper;
+import dev.xalpol12.wheretoeatbackend.service.mapper.PlaceMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class PlacesService {
     private final GeoApiContext geoApiContext;
+    private final PlaceMapper placeMapper;
+    private final PhotoMapper photoMapper;
 
     @Autowired
-    public PlacesService(GeoApiContext geoApiContext) {
+    public PlacesService(GeoApiContext geoApiContext, PlaceMapper placeMapper, PhotoMapper photoMapper) {
         this.geoApiContext = geoApiContext;
+        this.placeMapper = placeMapper;
+        this.photoMapper = photoMapper;
     }
 
-    public PlacesSearchResponse findPlaceByLocation(PlaceRequestDTO request) throws IOException, InterruptedException, ApiException {
-        return PlacesApi.nearbySearchQuery(geoApiContext, request.location())
+    public List<PlaceResponseDTO> findPlaceByLocation(PlaceRequestDTO request) throws IOException, InterruptedException, ApiException {
+        PlacesSearchResult[] response =  PlacesApi
+                .nearbySearchQuery(geoApiContext, request.location())
                 .radius(request.distance())
                 .rankby(RankBy.PROMINENCE)
                 .language("pl")
                 .minPrice(request.minPrice())
                 .maxPrice(request.maxPrice())
                 .type(request.type())
-                .await();
+                .await().results;
+        return placeMapper.placesSearchResultArrayToPlaceResponseDTOList(response);
     }
 
-    public ImageResult getPhotoUrl(PhotoRequestDTO request) throws IOException, InterruptedException, ApiException {
-        return PlacesApi.photo(geoApiContext, request.photoReference())
+    public PhotoResponseDTO getPhotoUrl(PhotoRequestDTO request) throws IOException, InterruptedException, ApiException {
+        ImageResult imageResult = PlacesApi
+                .photo(geoApiContext, request.photoReference())
                 .maxHeight(request.height())
                 .maxWidth(request.width())
                 .await();
+        return  photoMapper.imageResultToPhotoResponseDTO(imageResult);
     }
 }
