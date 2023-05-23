d6ba96cb73690462696e039b6c3a46fb0f79f713
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-05-23 15:20:24 +0200

    add responseDTO field: location of found place [lat, lng]

--- src/main/java/dev/xalpol12/wheretoeatbackend/service/dto/PlaceResponseDTO.java
+++ src/main/java/dev/xalpol12/wheretoeatbackend/service/dto/PlaceResponseDTO.java
@@ -1,5 +1,6 @@
 package dev.xalpol12.wheretoeatbackend.service.dto;
 
+import com.google.maps.model.LatLng;
 import lombok.*;
 
 @Data
@@ -9,6 +10,7 @@ import lombok.*;
 public class PlaceResponseDTO {
     private String name;
     private String placeId;
+    private LatLng location;
     private String vicinity;
     private float rating;
     private int userRatingsTotal;
